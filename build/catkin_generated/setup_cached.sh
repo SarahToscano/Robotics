e8128f2871257367ae1d8891ce3992ab5fafad8e
claudiosouzabrito <claudiosouzabrito@gmail.com>
2021-08-26 17:14:38 -0300

    teste

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/cloud/ROS/Robotics/devel:/home/cloud/ROS/catkin_ws2/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/home/cloud/ROS/catkin_ws2/devel/lib:/opt/ros/noetic/lib:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins'
+export PKG_CONFIG_PATH='/home/cloud/ROS/catkin_ws2/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig'
+export PWD='/home/cloud/ROS/Robotics/build'
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cloud/ROS/Robotics/devel/share/common-lisp:/home/cloud/ROS/catkin_ws2/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/cloud/ROS/Robotics/src:/home/cloud/ROS/catkin_ws2/src:/opt/ros/noetic/share'
