809ab7cf6b1a62b2eaaa3df7b2d353269b6ff795
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2021-08-26 17:44:17 -0300

    colocando workspace do ROS

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/cloud/ROS/Robotics/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/cloud/ROS/catkin_ws2/devel;/opt/ros/noetic'.split(';') if '/home/cloud/ROS/catkin_ws2/devel;/opt/ros/noetic' != '' else []
