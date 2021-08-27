e11b28685fb68c68354f3f6f485cb28a881564cd
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2021-08-26 22:01:55 -0300

    fatgrid

--- OG + rota/occupancyGrid.m
+++ OG + rota/occupancyGrid.m
@@ -5,3 +5,8 @@ grid = binaryOccupancyMap(binImage, 50);
 show(grid)
 saveas(gcf, "grid.bmp")
 
+robotRadius = 0.177;
+fatGrid = copy(grid);
+inflate(fatGrid, robotRadius);
+show(fatGrid)
+saveas(gcf, 'fatGrid.bmp')
